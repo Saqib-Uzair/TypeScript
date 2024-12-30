d16602cd83f2272b2af329cd260c97111d5fbae5
Saqib_Uzair <89041654+Saqib123-batman@users.noreply.github.com>
2024-12-31 02:06:57 +0500

    Add files via upload

---
+++ Learning/variable/05_let.ts
@@ -0,0 +1,4 @@
+// Create an array colors that holds a list of string values.
+
+let colors=["Black","Blue","Red","Green"];
+console.log(colors);
---
+++ Learning/variable/06_var.ts
@@ -0,0 +1,4 @@
+// Create an array scores that holds a list of numbers.
+
+var scores = [65,33,75,90];
+console.log(scores);
