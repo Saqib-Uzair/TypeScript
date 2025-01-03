86d248b8d0e05b7eeea9b0a720319e482800016c
Saqib_Uzair <89041654+Saqib123-batman@users.noreply.github.com>
2025-01-03 19:36:29 +0500

    Add files via upload

---
+++ Learning/function/10-function.ts
@@ -0,0 +1,19 @@
+// Practice: Write functions for addition, multiplication
+
+function addition(x:number,y:number){
+
+    return x + y;
+}
+
+
+function multiplication(x:number,y:number){
+
+    return x * y;
+}
+
+
+let add_two = addition(1,2);
+let mul_two = multiplication(5,10);
+
+console.log(add_two);
+console.log(mul_two);
