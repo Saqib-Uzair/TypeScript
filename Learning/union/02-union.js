c9106e088c8cf890fdf11b62ce96ed1aeaa7903a
Saqib Uzair <89041654+Saqib-Uzair@users.noreply.github.com>
2025-01-10 16:13:38 +0500

    Add files via upload

---
+++ Learning/union/02-union.js
@@ -0,0 +1,8 @@
+// Write a function printValue that accepts a parameter which can be either a string or a boolean.
+function printValue(x) {
+    return x;
+}
+var x_string = printValue("Saqib Uzair");
+var x_boolean = printValue(true);
+console.log(x_string);
+console.log(x_boolean);
