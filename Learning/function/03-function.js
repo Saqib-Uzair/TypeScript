d4ad032b72423a8b1947361b403d12e2b1eeffff
Saqib Uzair <89041654+Saqib-Uzair@users.noreply.github.com>
2025-01-13 17:00:14 +0500

    Delete Learning/function directory

--- Learning/function/03-function.js
+++
@@ -1,6 +0,0 @@
-// function to create and add two values 
-function addTwo(x, y) {
-    return x + y;
-}
-var sum = addTwo(1, 2);
-console.log(sum);
--- Learning/function/07-function.ts
+++
@@ -1,9 +0,0 @@
-// Write a function to pring the number 1 to 10
-
-function nPrint(){
-    for (let i=1 ; i<11; i ++){
-        console.log(i)
-    }
-}
-
-nPrint();
