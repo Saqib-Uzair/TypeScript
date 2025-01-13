f0f28731538a2446a4b1b75affc0927d826c40ad
Saqib Uzair <89041654+Saqib-Uzair@users.noreply.github.com>
2025-01-13 17:00:51 +0500

    Delete Learning/union directory

--- Learning/union/01-union.ts
+++
@@ -1,7 +0,0 @@
-// Create a variable data that can store either a string or a number.
-
-let data: string | number 
-data = "saqib"
-data = 25;
-
-console.log(data)
--- Learning/union/02-union.js
+++
@@ -1,8 +0,0 @@
-// Write a function printValue that accepts a parameter which can be either a string or a boolean.
-function printValue(x) {
-    return x;
-}
-var x_string = printValue("Saqib Uzair");
-var x_boolean = printValue(true);
-console.log(x_string);
-console.log(x_boolean);
