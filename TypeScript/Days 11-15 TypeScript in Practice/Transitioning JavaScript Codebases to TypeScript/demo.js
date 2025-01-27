2e692b7c4541d327744612cd41261088fce8c042
Saqib Uzair <89041654+Saqib-Uzair@users.noreply.github.com>
2025-01-28 01:52:40 +0500

    Add files via upload

---
+++ TypeScript/Days 11-15 TypeScript in Practice/Transitioning JavaScript Codebases to TypeScript/demo.js
@@ -0,0 +1,1 @@
+console.log("Demo: Transitioning JavaScript Codebases to TypeScript");
---
+++ TypeScript/Days 11-15 TypeScript in Practice/Writing Type-Safe Code and Error Handling/error-1.ts
@@ -0,0 +1,12 @@
+// basic error
+
+try{
+
+    throw new Error ("Something went wrong!");
+} catch (error) {
+
+    console.error("Caught an error:", error);
+} finally {
+    
+    console.log("This is optional: this will run no matter what");
+}
